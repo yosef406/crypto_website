ab26ac342b82c2c08544f20fae1ba2a647ccb465
Yosef Awad <yosef.awad.1@gmail.com>
2022-08-10 10:07:52 +0300

    feat: added about section

--- scripts/script.js
+++ scripts/script.js
@@ -17,11 +17,12 @@ $(document).ready(async () => {
     $("#aboutBtn").click(() => {
         clearInterval(interval);
         viewController
-            .switchView(viewController.View.ABOUT)
+            .switchView(viewController.View.ABOUT, buildAboutPage)
     });
 
     $("#searchBtn").click(searchCoins);
-
+    viewController
+        .switchView(viewController.View.ABOUT, buildAboutPage)
 });
 
 function searchCoins() {
@@ -224,7 +225,67 @@ function buildChart() {
 }
 
 function buildAboutPage() {
+    let aboutSection = $(`#aboutSection`);
+    let page = $(`<div class="page"></div>`)
+        .append(`<h1>About Us</h1>`).append(`<hr>`)
+        .append(`<div> <h2>Creator: </h2> <h3> Yosef Awad</h3> </div>`)
+        .append(`<div> <h2>Creator Email: </h2> <h3> Yosef.Awad.1@gmail.com</h3> </div>`)
+        .append("<hr>")
+        .append(`<div> <h3>About the website</h3> <br> </div>`)
+        .append(`<div><p>
+        Cryptonite is a website for watching and following information about crypto currency,<br>
+        here you will be able to browse and view different crypto currencies,you will see the currency name and its symbol,
+        you also have the option to view additional info like the image of the currency and its value in US Dollars, Euro and New Israeli Shekel.<br>
+        if you wish to follow a the rise and fall of a currency's value you ca do so with a toggle to add up to 
+        five to a live report that will display a cart with all currencies selected,
+        you will be able to see the live report chart by navigating to the live report section.<br>
+        </p></div><hr>`)
+        .append(`<div> <h3>Terms and conditions</h3> <br> </div>`)
+        .append(`<div><h4>Introduction</h4></div>`)
+        .append(`<div><p>
+        These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, 
+        Cryptonite accessible at Cryptonite.com.
+        These Terms will be applied fully and affect to your use of this Website. By using this Website, 
+        you agreed to accept all terms and conditions written in here. 
+        You must not use this Website if you disagree with any of these Website Standard Terms and Conditions.
+        Minors or people below 18 years old are not allowed to use this Website.
+        </p></div>`)
+        .append(`<div><h4>Intellectual Property Rights</h4></div>`)
+        .append(`<div><p>
+        Other than the content you own, under these Terms, 
+        Crypto Watch and/or its licensors own all the intellectual property rights and materials contained in this Website.
+        You are granted limited license only for purposes of viewing the material contained on this Website.
+        </p></div>`)
+        .append(`<div><h4>Restrictions</h4></div>`)
+        .append(`<div><p>You are specifically restricted from all of the following: <br></p></div>`)
+        .append(`<div><ul>
+        <li>publishing any Website material in any other media; </li>
+        <li>selling, sublicensing and/or otherwise commercializing any Website material; </li>
+        <li>publicly performing and/or showing any Website material; </li>
+        <li>using this Website in any way that is or may be damaging to this Website; </li>
+        <li>using this Website in any way that impacts user access to this Website; </li>
+        <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity; </li>
+        <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website; </li>
+        <li>using this Website to engage in any advertising or marketing. </li>
+        </ul></div>`)
+        .append(`<div><p>
+        Certain areas of this Website are restricted from being access by you and Company 
+        Name may further restrict access by you to any areas of this Website, at any time, 
+        in absolute discretion. Any user ID and password you may have for 
+        this Website are confidential and you must maintain confidentiality as well.
+        <br></p></div>`)
+        .append(`<div><h4>Your Content</h4></div>`)
+        .append(`<div><p>
+        In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, 
+        video text, images or other material you choose to display on this Website. 
+        By displaying Your Content, you grant Company Name a non-exclusive, 
+        worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, 
+        translate and distribute it in any and all media.<br>
+        Your Content must be your own and must not be invading any third-party's rights. 
+        Crypto Watch reserves the right to remove any of Your Content from this Website at any time without notice.
+        <br></p></div>`)
 
+    aboutSection.append(page);
 }
 
 function toggledPopUp(coin, toggleBtn) {
